e7cec3f02f96dfaff0810e025d352892de3060bb
EllaBEE02 <emmanuellabesiru@gmail.com>
2024-12-10 15:01:39 +0100

    Added basic search engine function (no API)

--- main/src/script.js
+++ main/src/script.js
@@ -1,1 +1,9 @@
+function displayCity(event) {
+  event.preventDefault();
+  let searchFormInput = document.querySelector("#search-form-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = searchFormInput.value;
+}
 
+let selectSearchForm = document.querySelector("#submit-form");
+selectSearchForm.addEventListener("submit", displayCity);
