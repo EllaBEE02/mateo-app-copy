7059de456f772d4c1cfe85e37e13db6379d3e080
EllaBEE02 <emmanuellabesiru@gmail.com>
2024-12-10 15:48:25 +0100

    Added search functionality using API

--- main/src/script.js
+++ main/src/script.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ca84b50945053937af3t3beb98o11a3c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function displayCity(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
   let city = document.querySelector("#city");
   city.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
 let selectSearchForm = document.querySelector("#submit-form");
